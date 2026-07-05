ed1c6703867fab0486be2ed666f6ff2b43454301
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-05 23:44:51 +0200

    modfiy album

---
+++ scripts/albumTemplate.js
@@ -0,0 +1,33 @@
+function createAlbumsHeader() {
+    return `
+        <div class="head">
+            <h1>ALBUMS JSON</h1>
+            <ul>
+                <li><a href="../index.html">Home</a></li>
+                <li><a href="pages/pages/posts.html">Posts</a></li>
+            </ul>
+        </div> 
+    `;
+}
+
+function createAlbums(albums) {
+    return `
+        <section class="usersCard">
+            <table>
+                <caption>
+                    <strong><h1>${albums.title}</h1></strong>
+                </caption>
+                <tbody>
+                    <tr>
+                        <th>USER ID</th>
+                        <td>${albums.userId}</td>
+                    </tr>
+                    <tr>
+                        <th>Title ID</th>
+                        <td>${albums.id}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </section>
+    `;
+}
