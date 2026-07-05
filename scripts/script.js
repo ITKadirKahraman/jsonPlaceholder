287f3710ff48bf1dad8fc7279d3c5d5c30127dfa
ITKadirKahraman <kadirkahraman.it@gmail.com>
2026-07-05 23:43:29 +0200

    modify script

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
 let users = [];
 let posts = [];
+let albums = [];
+let todos = [];
 
 function init() {
     renderHeader();
@@ -11,12 +13,30 @@ function initPost() {
     fetchDataJsonPost();
 }
 
+function initAlbums() {
+    renderAlbumHeader();
+    fetchDataJsonAlbum();
+}
+
+function initToDo() {
+    renderToDoHeader();
+    fetchDataJsonToDo();
+}
+
 function renderHeader() {
     document.getElementById('header').innerHTML = creatHeader();
 }
 
 function renderPostHeader() {
     document.getElementById('headPost').innerHTML = createPostHeader();
+}
+
+function renderAlbumHeader() {
+    document.getElementById('headAlbums').innerHTML = createAlbumsHeader();
+}
+
+function renderToDoHeader() {
+    document.getElementById('headToDo').innerHTML = createToDoHeader();
 }
 
 async function fetchDataJson() {
@@ -31,6 +51,20 @@ async function fetchDataJsonPost() {
     posts = await response.json();
     console.log(posts);
     renderPosts();
+}
+
+async function fetchDataJsonAlbum() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/albums');
+    albums = await response.json();
+    console.log(albums);
+    renderAlbums();
+}
+
+async function fetchDataJsonToDo() {
+    let response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    todos = await response.json();
+    console.log(todos);
+    renderToDos();
 }
 
 async function postData(url = "", data = {}) {
@@ -57,5 +91,21 @@ function renderPosts() {
     content.innerHTML = '';
     for (let index = 0; index < posts.length; index++) {
         content.innerHTML += createPosts(posts[index]);
+    }
+}
+
+function renderAlbums() {
+    let content = document.getElementById('albumsCard');
+    content.innerHTML = '';
+    for (let index = 0; index < albums.length; index++) {
+        content.innerHTML += createAlbums(albums[index]); 
+    }
+}
+
+function renderToDos() {
+    let content = document.getElementById('toDoCard');
+    content.innerHTML = '';
+    for (let index = 0; index < todos.length; index++) {
+        content.innerHTML += createToDo(albums[index]); 
     }
 }
